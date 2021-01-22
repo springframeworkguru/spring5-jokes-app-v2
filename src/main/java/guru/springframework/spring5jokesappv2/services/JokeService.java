9bdd63bad25945ad1238e14289997df0dbea52f1
John Thompson <john@springframework.guru>
2021-01-22 16:58:13 -0500

    adding Joke Service. Closes #3

---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring5jokesappv2.services;
+
+/**
+ * Created by jt on 1/22/21.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 1/22/21.
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
