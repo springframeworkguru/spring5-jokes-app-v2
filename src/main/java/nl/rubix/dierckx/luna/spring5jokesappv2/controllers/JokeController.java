7bad30ce6c6d7b75d8d547dd1f9e1b1cc69d461b
Luna Dierckx <luna.dierckx@rubix.nl>
2021-03-11 12:22:25 +0100

    added joke controller

---
+++ src/main/java/nl/rubix/dierckx/luna/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package nl.rubix.dierckx.luna.spring5jokesappv2.controllers;
+
+import nl.rubix.dierckx.luna.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
