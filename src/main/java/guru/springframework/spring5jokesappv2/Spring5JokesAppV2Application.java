33948ce064d0342322175b74c9350cd62c62ee32
J. Trpka <jeremy.trpka@perficient.com>
2021-03-15 12:16:37 -0400

    Initial commit.

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV2Application.class, args);
+	}
+
+}
