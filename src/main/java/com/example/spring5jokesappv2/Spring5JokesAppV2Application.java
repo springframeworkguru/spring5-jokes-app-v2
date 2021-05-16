c46c9d5648bf45e98e92b18d60669c55c406e294
Yuri Gogolitsyn <ygogol@hotmail.com>
2021-05-16 13:54:13 +0100

    Created project using Initializr

---
+++ src/main/java/com/example/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppV2Application.class, args);
+    }
+
+}
