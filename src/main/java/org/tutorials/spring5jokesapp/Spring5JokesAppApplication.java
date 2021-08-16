1dead9a2c8106daa2bdcc25c96e4623611058c26
dorakefala <dora.kefala@oddschecker.com>
2021-08-16 19:05:20 +0300

    Add initial files

---
+++ src/main/java/org/tutorials/spring5jokesapp/Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package org.tutorials.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppApplication.class, args);
+	}
+
+}
