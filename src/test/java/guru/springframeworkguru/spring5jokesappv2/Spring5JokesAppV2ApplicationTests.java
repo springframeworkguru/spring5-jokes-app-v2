4ba765f8f33b38c3b71efe0103584fd0703d7ebf
Miklos Balazsi <miklos.balazsi@sagemcom.com>
2021-12-21 17:25:48 +0100

    Spring initilzr new gradle project Java 11. Closes #1

---
+++ src/test/java/guru/springframeworkguru/spring5jokesappv2/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframeworkguru.spring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
