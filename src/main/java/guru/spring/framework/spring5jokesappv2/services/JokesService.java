b9c76009559c84b5de04983a83db9faf055e6823
ashvinalex <ashvinalex@gmail.com>
2022-01-27 16:07:42 +0530

    added Service

---
+++ src/main/java/guru/spring/framework/spring5jokesappv2/services/JokesService.java
@@ -0,0 +1,5 @@
+package guru.spring.framework.spring5jokesappv2.services;
+
+public interface JokesService {
+    String getJoke();
+}
---
+++ src/main/java/guru/spring/framework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.spring.framework.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl implements JokesService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJoke() {
+
+        return chuckNorrisQuotes.getRandomQuote() ;
+    }
+}
