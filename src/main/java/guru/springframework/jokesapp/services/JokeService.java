f354071af7f663e22e38791d49033a1966198e71
Dmitry E <KeDeVe@gmail.com>
2022-12-25 23:22:23 -0500

    04.057. Creating the Spring Service Layer Add JokeService and JokeServiceImpl

---
+++ src/main/java/guru/springframework/jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
