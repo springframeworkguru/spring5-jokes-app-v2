c735355ab5a073c5868eb81d8ed647ef6aeda293
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-18 03:28:19 +0700

    Adding Joke Service. Closes #3

---
+++ src/main/java/com/vnlemanhthanh/springframeworkguruspring5jokesappv2/service/JokeService.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.springframeworkguruspring5jokesappv2.service;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/com/vnlemanhthanh/springframeworkguruspring5jokesappv2/service/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.springframeworkguruspring5jokesappv2.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
