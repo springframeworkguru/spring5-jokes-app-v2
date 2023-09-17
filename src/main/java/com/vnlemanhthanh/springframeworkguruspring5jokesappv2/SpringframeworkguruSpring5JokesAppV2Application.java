19c4bbd129312dab5d08f1b5d421255a24750d9e
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-09-18 02:52:49 +0700

    @Grimm33 Init Project. Closes springframeworkguru#1

---
+++ src/main/java/com/vnlemanhthanh/springframeworkguruspring5jokesappv2/SpringframeworkguruSpring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.springframeworkguruspring5jokesappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkguruSpring5JokesAppV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkguruSpring5JokesAppV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/vnlemanhthanh/springframeworkguruspring5jokesappv2/SpringframeworkguruSpring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.springframeworkguruspring5jokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkguruSpring5JokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
